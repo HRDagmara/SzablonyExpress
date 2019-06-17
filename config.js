5bd7ef81adca893caba8c350b31654569ed8be76
Dagmara Bełczowska <dagmara.belczowska@wp.pl>
2019-06-18 01:20:22 +0200

    Add authorization Google

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '504838829790-jeklmnrgq1i79u0lgtcd2tg7kg6ojgra.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'r6h6C5at8x2xSXrOkEbhDz3F',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+}
